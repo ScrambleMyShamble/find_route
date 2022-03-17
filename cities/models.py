335bd2833492d34bbb8716e9d4cf3fb610bc90ec
major <majorofffpavel@gmail.com>
2022-03-17 17:11:00 +0300

    add templates

--- cities/models.py
+++ cities/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class Cities(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Город')
 
     def __str__(self):
         return self.name
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import City
+
+__all__ = (
+    'home'
+)
+
 
 # Create your views here.
+def home(requests, pk=None):
+    if pk:
+        city = get_object_or_404(City, id=pk)
+        context = {'object': city}
+        return render(requests, 'cities/detail.html', context)
+
+    all_city_objects = City.objects.all()
+    context = {'objects_list': all_city_objects}
+    return render(requests, 'cities/home.html', context)
