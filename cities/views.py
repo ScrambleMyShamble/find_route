94f30074ca72df09eb1103646522056bca44ea85
major <majorofffpavel@gmail.com>
2022-03-18 09:54:40 +0300

    add form

--- cities/views.py
+++ cities/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import City
 from django.views.generic import DetailView
-
-__all__ = (
-    'home',
-    'CityDetailView'
-)
+from .forms import CityForm
 
 
 # Create your views here.
-def home(requests):
+def home(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = CityForm
     all_city_objects = City.objects.all()
-    context = {'objects_list': all_city_objects}
-    return render(requests, 'cities/home.html', context)
+    context = {'objects_list': all_city_objects, 'form': form}
+    return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
