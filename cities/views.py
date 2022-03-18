20e36ae71ec327668534f36639008d40c1abfe11
major <majorofffpavel@gmail.com>
2022-03-18 10:56:30 +0300

    add updateview deleteview

--- cities/views.py
+++ cities/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 
 from .models import City
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from .forms import CityForm
 
 
@@ -28,4 +28,16 @@ class CityCreateView(CreateView):
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
-    success_message = 'Город успешно создан'
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
